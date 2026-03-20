164f4a247d9462a2bebd9392f8f7d1eb7fd7de80
Sujal Bhardwaj <bhardwaj0601@gmail.com>
2026-03-20 14:41:38 +0530

    backend setup complete

---
+++ backend/server.js
@@ -0,0 +1,55 @@
+import dotenv from 'dotenv';
+dotenv.config({ quiet: true });
+
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import errorHandler from './middlewares/errorHandler.middleware.js';
+
+// ES6 module__dirname alternative
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Intialize app using express
+const app = express();
+
+//Connect to mongoDB
+
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from the uploads directory
+app.use(/uploads/, express.static(path.join(__dirname, 'uploads')));
+
+//Routes
+
+
+app.use(errorHandler);
+
+
+// 404 handler for undefined routes
+app.use((req, res)=>{
+    res.status(404).json({ message: 'Route not found' });
+});
+
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+process.on('unhandledRejection', (err) => {
+   console.error('Unhandled Rejection:${err.message}');
+   process.exit(1);
+});
+
